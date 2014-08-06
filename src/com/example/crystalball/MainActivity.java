943c0b694087bbe6a7b334ecfdf16febf8c3c9d3
Derek Liang <frostyrusty@gmail.com>
2014-08-06 15:33:28 -0700

    added sensors for shaking the app

--- src/com/example/crystalball/MainActivity.java
+++ src/com/example/crystalball/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.crystalball;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
@@ -13,12 +15,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.crystalball.ShakeDetector.OnShakeListener;
+
 public class MainActivity extends Activity {
 
 	private CrystalBall mCrystalBall = new CrystalBall();
 	private TextView mAnswerLabel;
 	private Button mGetAnswerButton;
 	private ImageView mCrystalBallImage;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,15 @@ public class MainActivity extends Activity {
 		mAnswerLabel = (TextView) findViewById(R.id.textView1);
 		mGetAnswerButton = (Button) findViewById(R.id.button1);
 		mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
+		 
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mShakeDetector = new ShakeDetector(new OnShakeListener() {
+			@Override
+			public void onShake() {
+				handleNewAnswer();
+			}
+		});
 		
 		// EXTRA CREDIT: Change background color on click
 		//final RelativeLayout layout = (RelativeLayout) findViewById(R.id.relativeLayout1);
@@ -36,21 +52,23 @@ public class MainActivity extends Activity {
 		mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String answer = mCrystalBall.getAnAnswer();
-
-				// Update the label with our dynamic answer
-				mAnswerLabel.setText(answer);
-				
-				// EC1:
-				//layout.setBackgroundColor(Color.GREEN);
-				
-				animateCrystalBall();
-				animateAnswer();
-				playSound();
+				handleNewAnswer();
 			}
 		});
 	}
 
+	@Override
+	public void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer,
+				SensorManager.SENSOR_DELAY_UI);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(mShakeDetector);
+	}
 	
 	private void animateCrystalBall()
 	{
@@ -100,5 +118,17 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+
+	private void handleNewAnswer() {
+		String answer = mCrystalBall.getAnAnswer();
+
+		// Update the label with our dynamic answer
+		mAnswerLabel.setText(answer);
+		
+		animateCrystalBall();
+		animateAnswer();
+		playSound();
 	}
 }
