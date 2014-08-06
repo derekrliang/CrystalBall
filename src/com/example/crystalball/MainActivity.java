0df92743961b5922d208aca855cd841a1eb9a1c2
Derek Liang <frostyrusty@gmail.com>
2014-08-06 15:08:01 -0700

    added picture animation, text fade, and sounds

--- src/com/example/crystalball/MainActivity.java
+++ src/com/example/crystalball/MainActivity.java
@@ -1,45 +1,88 @@
 package com.example.crystalball;
 
 import android.app.Activity;
-import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private CrystalBall mCrystalBall = new CrystalBall();
+	private TextView mAnswerLabel;
+	private Button mGetAnswerButton;
+	private ImageView mCrystalBallImage;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		// Declare our View variables and assign them the Views from the layout file
-		final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-		Button getAnswerButton = (Button) findViewById(R.id.button1);
+		// Assigning our View variables from the layout file
+		mAnswerLabel = (TextView) findViewById(R.id.textView1);
+		mGetAnswerButton = (Button) findViewById(R.id.button1);
+		mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
 		
 		// EXTRA CREDIT: Change background color on click
 		//final RelativeLayout layout = (RelativeLayout) findViewById(R.id.relativeLayout1);
 		
-		getAnswerButton.setOnClickListener(new View.OnClickListener() {
+		mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				String answer = mCrystalBall.getAnAnswer();
 
 				// Update the label with our dynamic answer
-				answerLabel.setText(answer);
+				mAnswerLabel.setText(answer);
 				
 				// EC1:
 				//layout.setBackgroundColor(Color.GREEN);
+				
+				animateCrystalBall();
+				animateAnswer();
+				playSound();
 			}
 		});
 	}
 
+	
+	private void animateCrystalBall()
+	{
+		mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+		AnimationDrawable ballAnimation = (AnimationDrawable) mCrystalBallImage.getDrawable();
+		
+		if (ballAnimation.isRunning()) {
+			ballAnimation.stop();
+		}
+		
+		ballAnimation.start();
+	}
+	
+	private void animateAnswer() {
+		AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+		fadeInAnimation.setDuration(1500);
+		fadeInAnimation.setFillAfter(true);
+		
+		mAnswerLabel.setAnimation(fadeInAnimation);
+	}
+	
+	private void playSound() {
+		MediaPlayer player = MediaPlayer.create(this, R.raw.crystal_ball);
+		player.start();
+		player.setOnCompletionListener(new OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+			}
+		});	
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
